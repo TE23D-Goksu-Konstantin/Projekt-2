566344657a704d37ef9e4b7577f95804aa7e6570
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2025-03-17 09:44:14 +0100

    end of lesson commit

--- Projekt 2/obj/Debug/net8.0/Projekt 2.AssemblyInfo.cs
+++ Projekt 2/obj/Debug/net8.0/Projekt 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekt 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ef51f5a5872d7a44df8f37afc4dcd4bd9de658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c3c6d96e939522390dc34376cd111318318b20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekt 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekt 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
