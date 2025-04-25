d064003bedcb04121befdec05b514bb8bf38fa8b
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2025-04-25 10:47:44 +0200

    Comments + other

--- Projekt 2/Program.cs
+++ Projekt 2/Program.cs
@@ -1,2 +1,9 @@
 ﻿Battle battle = new Battle();
-battle.StartBattle();
+battle.StartBattle(); //Begins the battle
+Console.Clear();
+
+string consoleOutput = "The Battle has ended, you've won";
+Utility.writing(consoleOutput);
+
+Thread.Sleep(1000);
+Environment.Exit(0);
